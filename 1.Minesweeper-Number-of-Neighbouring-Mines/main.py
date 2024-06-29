789e4b81fae5d0b4075f55c422e46f89e591af0a
RafaelBicas <rafaelbicas@gmail.com>
2024-06-29 19:24:52 -0300

    Re-reading the exercise, it was known that it was asked for just one function that performs all the work. It was created a new one for that and updated the exercise.

--- 1.Minesweeper-Number-of-Neighbouring-Mines/main.py
+++ 1.Minesweeper-Number-of-Neighbouring-Mines/main.py
@@ -1,8 +1,8 @@
 ###############INPUT################
 board = [
-    [1, 1, 1, 0],
-    [1, 0, 1, 0],
-    [1, 1, 1, 1],
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 1, 0, 1],
     [1, 1, 0, 0]
 ]
 ###############INPUT################
@@ -18,45 +18,34 @@ def check_input_values(input):
                 return False
     return True
 
-def check_neighbour_exists(board, x, y): 
-    return 0 <= x < len(board) and 0 <= y < len(board[0]) #Verifying if the value being compared is in/out of scope
-                
-def identify_neighbour_values(board, x, y):
-    
-    for i in range(x-1, x+2):
-        for j in range(y-1, y+2):
-            if(check_neighbour_exists(board, i, j) and board[i][j] != MINE_SPACE):
-                board[i][j] += 1
-    
-    return board
-
-def identify_mines(board):
+def identify_mines_and_neighbour_values(board):
     bombs_board = []
 
-    for x in range(0,len(board)): 
+    for x in range(0, len(board)):
         new_row = []
         for y in range(0, len(board[x])):
-            if(board[x][y] == 0):
+            if board[x][y] == 0:
                 new_row.append(EMPTY_SPACE)
             else: #The space being verified has a bomb
                 new_row.append(MINE_SPACE)
         bombs_board.append(new_row)
 
+    for x in range(0, len(bombs_board)):
+        for y in range(0, len(bombs_board[x])):
+            if bombs_board[x][y] == MINE_SPACE:
+                for i in range(x-1, x+2):
+                    for j in range(y-1, y+2):
+                        if 0 <= i < len(bombs_board) and 0 <= j < len(bombs_board[0]) and bombs_board[i][j] != MINE_SPACE:
+                            bombs_board[i][j] += 1
+
     return bombs_board
-    
 
 def main():
     if not check_input_values(board):
         print("Invalid board. The board should only contain 0s and 1s.")
         return
-    
-    new_board = identify_mines(board)
 
-    for x in range(0,len(board)): 
-        for y in range(0, len(board[x])):
-            if board[x][y] == 1:
-                new_board = identify_neighbour_values(new_board, x, y)
-            
+    new_board = identify_mines_and_neighbour_values(board)
     for row in new_board:
         print(row)
 
